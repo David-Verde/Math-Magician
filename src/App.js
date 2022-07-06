1faf56b42d0147a3d361ac32b7b409037ec399f3
Unyielding1 <david.verde.alvarez@gmail.com>
2022-07-06 10:44:31 -0500

    add pages and style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
 import './App.css';
+
+import Navbar from './components/Navbar';
+
+import Home from './pages/Home';
 import Calculator from './components/calculator';
+import Quote from './pages/Quote';
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <div className="container hasNavbar">
+            <Route path="/calculator">
+              <Calculator />
+            </Route>
+            <Route path="/quote">
+              <Quote />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </div>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => (
+  <>
+    <h1>Math Magician App!</h1>
+    {/* eslint-disable-next-line */}
+    <p>This App was Created using React, and Microverse Tutorials</p>
+    {/* eslint-disable-next-line */}
+    <p>Dictum non consectetur a erat nam at lectus. In ante metus dictum at tempor commodo. Quisque id diam vel quam elementum pulvinar etiam non. Interdum velit euismod in pellentesque massa placerat duis ultricies. Erat velit scelerisque in dictum non. Tellus rutrum tellus pellentesque eu tincidunt tortor aliquam. Fringilla ut morbi tincidunt augue interdum velit. A condimentum vitae sapien pellentesque habitant morbi tristique senectus et. Nunc aliquet bibendum enim facilisis gravida neque convallis a cras. Lorem ipsum dolor sit amet consectetur. Integer malesuada nunc vel risus commodo viverra maecenas accumsan lacus. Arcu cursus euismod quis viverra nibh cras pulvinar.</p>
+  </>
+);
+
+export default Home;
