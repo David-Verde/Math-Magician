887c7ef9d6cd33ea6f148d62d4de61efe23be327
Unyielding1 <david.verde.alvarez@gmail.com>
2022-07-07 09:51:22 -0500

    remove ja extention

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 
 import Home from './pages/Home';
-import Calculator from './components/Calculator.js';
+import Calculator from './components/Calculator';
 import Quote from './pages/Quote';
 
 function App() {
--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Calculator from '../components/Calculator.js';
+import Calculator from '../components/Calculator';
 
 it('renders correctly', () => {
   const tree = renderer
